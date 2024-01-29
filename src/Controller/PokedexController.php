907bde3d71833d7d1fd57ae724711d7a7c46c7fb
Robert Blackburn <professorblackers@gmail.com>
2024-01-29 23:08:40 +0100

    Add initial packages, controller, template and icon

---
+++ src/Controller/PokedexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PokedexController extends AbstractController
+{
+    #[Route('/', name: 'pokedex')]
+    public function index(): Response
+    {
+        return $this->render('pokedex/index.html.twig', [
+        ]);
+    }
+}

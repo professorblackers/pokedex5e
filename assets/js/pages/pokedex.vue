e55b787335d22ce3ee3fe0874fe30c9deebfcb78
Robert Blackburn <professorblackers@gmail.com>
2024-02-28 22:25:26 +0100

    Several updates to Pokedex page; add pokedex header containing image, name, number, stage & sr, including adaptive background; add pokedex containers currently containing basic pokemon and dnd info; add pokedex footer to contain next and previous buttons

--- assets/js/pages/pokedex.vue
+++ assets/js/pages/pokedex.vue
@@ -1,10 +1,54 @@
 <template>
-  <h2 v-if="pokemon.length >0">#{{ pokemon[index].pokedexNumber }}</h2>
-  <h1 v-if="pokemon.length >0">{{ pokemon[index].name }}</h1>
-  <img v-if="pokemon.length >0" :src="getPokemonImage"  data-adaptive-background="" class="pokemon-image">
-  <div>
-    <button @click="previousPokemon()" :disabled="isDisabled('previous')">Previous</button>
-    <button @click="nextPokemon()" :disabled="isDisabled('next')">Next</button>
+
+  <div class="pokedex-header">
+    <img v-if="pokemon.length >0" :src="getPokemonImage" data-adaptive-background="" :alt="pokemon[index].name" style="padding-bottom: 20px; padding-top: 10px; width: 5%; height: auto">
+    <h1 v-if="pokemon.length >0">{{ pokemon[index].name }}</h1>
+    <h1 class="left-item" v-if="pokemon.length >0">#{{ pokemon[index].pokedexNumber }}</h1>
+    <h1 class="right-item" v-if="pokemon.length >0">Stage {{ pokemon[index].stage }} | SR {{ pokemon[index].speciesRating }}</h1>
+  </div>
+
+  <div class="row" style="max-width: 100%">
+    <div class="col-md-4">
+      <div class="pokedex-container">
+        <div>
+          <p v-if="pokemon[index].secondaryType">Type: {{ pokemon[index].primaryType }} & {{ pokemon[index].secondaryType }}</p>
+          <p v-else>Type: {{ pokemon[index].primaryType }}</p>
+          <p>Resistance: {{ pokemon[index].resistance }}</p>
+          <p>Immune: {{ pokemon[index].immune }}</p>
+          <p>Weakness: {{ pokemon[index].vulnerable }}</p>
+          <p v-if="pokemon[index].abilityTwo">Abilities: {{ pokemon[index].abilityOne }} & {{ pokemon[index].abilityTwo }}</p>
+          <p v-else>Ability: {{ pokemon[index].abilityOne }}</p>
+          <p>Hidden Ability: {{ pokemon[index].hiddenAbility }}</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="pokedex-container">
+        <div>
+          <p>AC: {{ pokemon[index].armorClass }}</p>
+          <p>Hit Points: {{ pokemon[index].hitPoints }} | Hit Dice: {{ pokemon[index].hitDice }}</p>
+          <p>Strength: {{ pokemon[index].strength }}</p>
+          <p>Dexterity: {{ pokemon[index].dexterity }}</p>
+          <p>Constitution: {{ pokemon[index].constitution }}</p>
+          <p>Intelligence: {{ pokemon[index].intelligence }}</p>
+          <p>Wisdom: {{ pokemon[index].wisdom }}</p>
+          <p>Charisma: {{ pokemon[index].charisma }}</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="pokedex-container">
+        <div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="pokedex-footer">
+    <button class="left-item" @click="previousPokemon()" :disabled="isDisabled('previous')">Previous</button>
+    <button class="right-item" @click="nextPokemon()" :disabled="isDisabled('next')">Next</button>
   </div>
 </template>
 
@@ -14,13 +58,19 @@ export default {
   data() {
     return {
       pokemon: [],
-      index: 0,
+      index: 0
     };
   },
   computed: {
     getPokemonImage() {
-      return '/assets/images/pokemon/'+this.pokemon[this.index].pokedexNumber+this.pokemon[this.index].name+'.png';
+      return '/assets/images/sprites/'+this.pokemon[this.index].pokedexNumber+'.png';
     },
+    getPrimaryType() {
+      return '/assets/images/types/'+this.pokemon[this.index].primaryType+'.png';
+    },
+    getSecondaryType() {
+      return '/assets/images/types/'+this.pokemon[this.index].secondaryType+'.png';
+    }
   },
   methods: {
     getPokemon: function() {
@@ -34,7 +84,22 @@ export default {
     },
     getBackground: function () {
       $(document).ready(function(){
-        $.adaptiveBackground.run();
+        $('img.pokemon-image').on('ab-color-found', function(){
+          var currentBackgroundColor = $('img.pokemon-image').attr('data-ab-color');
+          $('.pokemon-header').css('background-color', currentBackgroundColor);
+        });
+        var defaults = {
+          exclude: [
+            'rgb(0,0,0)',
+            'rgb(16,16,16)',
+            'rgb(255,255,255)',
+            'rgb(24,24,24)',
+            'rgb(238,238,255)',
+            'rgb(238,238,238)',
+            'rgb(246,246,255)'
+          ],
+        }
+        $.adaptiveBackground.run(defaults);
       });
     },
     previousPokemon() {
@@ -64,20 +129,45 @@ export default {
 
 body {
   font-family: 'Russo One', sans-serif;
-  color: white;
-  text-shadow: 2px 2px 4px #000000;
+  background: #ECE9E6;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #FFFFFF, #ECE9E6);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #FFFFFF, #ECE9E6); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 
 #pokedex {
-  height: 100vH;
-  padding-right: 15px;
-  padding-left: 15px;
-  margin-right: auto;
-  margin-left: auto;
+  height: 100vh;
 }
 
-.pokemon-image {
-  max-width: 400px;
-  height: auto;
+.pokedex-header {
+  color: white;
+  text-shadow: 2px 2px 4px #000000;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(141, 206, 193);
+}
+
+.pokedex-footer {
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  padding-bottom: 3rem;
+}
+
+.pokedex-container {
+  display: flex;
+  padding-left: 1rem;
+  padding-top: 1rem;
+}
+
+.left-item {
+  position: absolute;
+  left: 5rem
+}
+
+.right-item {
+  position: absolute;
+  right: 5rem
 }
 </style>
